424c35f850055361a533bd62d816491733f482d4
Auios <LoneAuios@gmail.com>
2020-12-25 11:14:00 -0500

    2020-3 Part 2

--- Advent-of-Code/2020/_3_Tobogan_Trajectory/Program.cs
+++ Advent-of-Code/2020/_3_Tobogan_Trajectory/Program.cs
@@ -11,17 +11,61 @@ namespace _3_Tobogan_Trajectory
 
         static void Main(string[] args)
         {
+            bool partTwo = true;
+
             map = File.ReadAllLines("input.txt");
-            x = y = 0;
 
-            int count = 0;
+            int result = 0;
 
-            while(Step())
+            if(partTwo)
             {
-                count += IsTree() ? 1 : 0;
-            }
+                x = y = 0;
+                int result1 = 0;
+                while(Step(1, 1))
+                {
+                    result1 += IsTree() ? 1 : 0;
+                }
 
-            Console.WriteLine(count);
+                x = y = 0;
+                int result2 = 0;
+                while(Step(3, 1))
+                {
+                    result2 += IsTree() ? 1 : 0;
+                }
+
+                x = y = 0;
+                int result3 = 0;
+                while(Step(5, 1))
+                {
+                    result3 += IsTree() ? 1 : 0;
+                }
+
+                x = y = 0;
+                int result4 = 0;
+                while(Step(7, 1))
+                {
+                    result4 += IsTree() ? 1 : 0;
+                }
+
+                x = y = 0;
+                int result5 = 0;
+                while(Step(1, 2))
+                {
+                    result5 += IsTree() ? 1 : 0;
+                }
+
+                result = result1 * result2 * result3 * result4 * result5;
+            }
+            else
+            {
+                x = y = 0;
+                while(Step(3, 1))
+                {
+                    result += IsTree() ? 1 : 0;
+                }
+            }
+            
+            Console.WriteLine(result);
         }
 
         static bool IsTree()
@@ -36,10 +80,10 @@ namespace _3_Tobogan_Trajectory
             return result;
         }
 
-        static bool Step()
+        static bool Step(int stepX, int stepY)
         {
-            x += 3;
-            y += 1;
+            x += stepX;
+            y += stepY;
 
             bool result = y < map.Length;
             if(result)
